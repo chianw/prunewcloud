4e0bc54948a9f0b2f2ecb6b1a0c892e41cf66cf7
chiancw <chiancw@gmail.com>
2025-01-02 13:46:29 +0800

    added additional outputs to stage0b

--- stage0b/outputs.tf
+++ stage0b/outputs.tf
@@ -1,3 +1,23 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.example.id
+}
+
 output "storage_account_id" {
   value = azurerm_storage_account.example.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
+
+output "storage_container_id" {
+  value = azurerm_storage_container.example.id
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.example.name
 }
